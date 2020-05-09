d85bb7b7c1dcf43987001f3ce0f89bea2205751d
bmbrgm <bmbrgm@bmb>
2020-05-09 23:08:12 +0530

    Test3

--- MytestProject/src/bm/Test.java
+++ MytestProject/src/bm/Test.java
@@ -4,7 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("masum");
+		int a=7;
+			System.out.println("masum New ");
+    System.out.println("masum ");
 	}
 
 }
